9f61a6da2b248b576c34772aa3301433f8fec053
Nurul Islam <nurul.islam96@aol.com>
2022-10-11 03:53:32 +0600

    topics added

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -6,7 +6,7 @@ const Topics = () => {
     const topics = useLoaderData()
     return (
         <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-            <div className='grid gap-8 row-gap-5 mb-8 lg:grid-cols-4 lg:row-gap-8'>
+            <div className='grid gap-8 row-gap-5 mb-8 lg:grid-cols-2 lg:row-gap-8'>
             {
                 topics.map(topic => <Topic topic={topic} key={topic.id}></Topic>)
             }
