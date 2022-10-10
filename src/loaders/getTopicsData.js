fd557286231810b67109ee1777bd55d70b5fceef
Nurul Islam <108375316+NurulIslam96@users.noreply.github.com>
2022-10-11 05:32:20 +0600

    Delete src directory

--- src/loaders/getTopicsData.js
+++
@@ -1,6 +0,0 @@
-export const getTopicsData = async () => {
-    const topicsData = await fetch('https://openapi.programming-hero.com/api/quiz')
-    const topicsJson = await topicsData.json()
-    const topics = topicsJson.data
-    return topics
-}
