b3349a1ddf6635626c43d2812a265a2281a4c22b
Nurul Islam <nurul.islam96@aol.com>
2022-10-11 22:39:23 +0600

    added home image along with buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
-import Topics from './Topics';
+import React from "react";
+import { Link } from "react-router-dom";
+import Topics from "./Topics";
 
 const Home = () => {
-    return (
-        <div>
-            <div className='flex'>
-                Hello
-                <img src="https://merakiui.com/images/header-image.svg" className='w-1/3' alt="" />
+  return (
+    <div>
+      <div className="bg-slate-800">
+          <div className="container text-center md:text-left mx-auto flex md:flex-row flex-col">
+            <div className="flex flex-col px-4 pt-16 lg:pb-56 md:py-32 md:px-10 lg:px-32">
+              <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-white">
+                QUIZZY POP
+              </h1>
+              <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-slate-200">
+                Best E-commerce platform for buying high quality Smart Home
+                Appliances at extremely affordable Price.
+              </p>
+              <div className="flex flex-wrap md:justify-start justify-between">
+                <Link to="/topics">
+                  <button
+                    type="button"
+                    className="px-8 py-3 text-lg font-semibold rounded-full  text-gray-900 hover:bg-cyan-400 bg-cyan-200"
+                  >
+                    Topics
+                  </button>
+                </Link>
+                <Link to="/blog">
+                  <button
+                    type="button"
+                    className="px-8 py-3 text-lg md:mx-5 border rounded-full border-cyan-400 text-white"
+                  >
+                    Learn more
+                  </button>
+                </Link>
+              </div>
             </div>
-            <Topics></Topics>
+            <img
+              src={`https://merakiui.com/images/header-image.svg`}
+              alt=""
+              className="md:w-1/3 w-2/3 mx-auto mb-5"
+            />
+          </div>
         </div>
-    );
+      <Topics></Topics>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const Topic = ({ topic }) => {
     const { name, id, logo, total } = topic;
     return (
-        <div className="md:p-5 shadow-slate-300 shadow-lg rounded-md">
-            <img className="bg-blue-100 md:w-full" src={logo} alt={name} />
-            <div className="md:flex md:text-left text-center justify-between">
-                <div className="justify-center my-2 select-none flex">
-                    <div>
+        <div className="md:p-5 bg-white shadow-slate-400 shadow-lg rounded-md">
+            <img className="bg-blue-200 md:w-full rounded-md" src={logo} alt={name} />
+            <div className="text-center justify-between">
+                <div className="justify-center my-2 mx-2 select-none flex">
+                    <div className="flex w-full items-end justify-between">
                         <p className="text-2xl font-semibold">{name}</p>
-                        <p className="font-semibold">Number of Qs: {total}</p>
+                        <p className="font-semibold text-slate-500">Questions: {total}</p>
                     </div>
                 </div>
-                <div className="justify-center mt-5 mb-2 select-none flex">
-                    <button className="py-2 px-4 shadow-md no-underline rounded-md bg-slate-500 text-white font-sans font-semibold text-sm border-blue hover:text-white hover:bg-slate-700 focus:outline-none active:shadow-none"><Link to={`/topics/${id}`}>Lets Practice</Link></button>
+                <div className="justify-center mt-2 mb-2 select-none flex">
+                    <button className="md:py-2 py-3 mx-2 w-full shadow-md no-underline rounded-md bg-slate-500 text-white font-sans font-semibold md:text-lg text-2xl border-blue hover:text-white hover:bg-slate-700 focus:outline-none active:shadow-none"><Link to={`/topics/${id}`}>Lets Practice</Link></button>
                 </div>
             </div>
         </div>
