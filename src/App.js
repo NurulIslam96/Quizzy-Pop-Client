5919441f4f0105a7ae80f93901c21e3f025fad5d
Nurul Islam <nurul.islam96@aol.com>
2022-10-12 03:45:00 +0600

    added charts in statistics section

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       children:[
         {path: '/', element:<Home></Home>, loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz')},
         {path:'/topics', element: <Topics></Topics>, loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz')},
-        {path:'/statistics',element:<Statistics></Statistics>},
+        {path:'/statistics',element:<Statistics></Statistics>, loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz')},
         {path:'/blog', element:<Blog></Blog>},
         {path:'/topics/:topicId', element: <QuizDetails></QuizDetails>, loader: async({params})=> fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)}
       ]
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import { Tooltip } from "chart.js";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    XAxis,
+    YAxis
+} from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>   
-            <h2>This is Statis</h2>
+  const questionsData = useLoaderData();
+  const { data } = questionsData;
+  console.log(data);
+  return (
+    <div className="mt-48 md:w-1/2">
+        <div>
+            
         </div>
-    );
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="name" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Statistics;
